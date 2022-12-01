18bdecbd3f259c21e1eb3176aa6f37d3f82cede5
shivangilluminz <shivang@illuminz.com>
2022-12-01 21:56:52 +0530

    Added Test Cases

--- test/unit/FundMe.test.js
+++ test/unit/FundMe.test.js
@@ -1,4 +1,4 @@
-const { assert } = require("chai");
+const { assert, expect } = require("chai");
 const { deployments, getNamedAccounts, ethers } = require("hardhat");
 
 
@@ -6,6 +6,7 @@ describe("FundMe", async () => {
     let fundMe = null;
     let deployer = null;
     let mockV3Aggregator = null;
+    const sendValue = ethers.utils.parseEther("1")
     beforeEach(async () => {
         //deploye fundme
         deployer = (await getNamedAccounts()).deployer
@@ -17,6 +18,44 @@ describe("FundMe", async () => {
         it("Set the aggregator correctly", async () => {
             const response = await fundMe.priceFeed();
             assert.equal(response, mockV3Aggregator.address)
+        })
+    })
+
+    describe("fund Function Testing", () => {
+        it("Fails if you don't send enough ethers", async () => {
+            await expect(fundMe.fund()).to.be.revertedWith("You are not eligible yet for donate minimum amount of fund....")
+        })
+        it("Updates The Amount funded data Structure", async () => {
+            await fundMe.fund({ value: sendValue })
+            const response = await fundMe.addressToAmountFunder(deployer)
+            assert.equal(response.toString(), sendValue.toString())
+        })
+        it("Add Funders to funders Array", async () => {
+            await fundMe.fund({ value: sendValue })
+            const response = await fundMe.funders(0)
+            assert.equal(response, deployer)
+        })
+        describe("withdraw", async () => {
+            beforeEach(async () => {
+                await fundMe.fund({ value: sendValue })
+            })
+        })
+        it("Withdraw ETH from a single founder", async () => {
+            //Arrange
+            const startingFundmebalance = await fundMe.provider.getBalance(fundMe.address)
+            const startingDeployerbalance = await fundMe.provider.getBalance(deployer)
+            // Act or Widraw functions
+            const actionresponse = await fundMe.widraw();
+            const transactionRecipt = await actionresponse.wait(1)
+            // calculating Gas Cost for transaction
+            const { gasUsed, effectivegasPrice } = transactionRecipt
+            const gasCost = gasUsed.mul(effectivegasPrice)
+            //Get Remaining Balance
+            const endFundmebalance = await fundMe.provider.getBalance(fundMe.address)
+            const endDeployerbalance = await fundMe.provider.getBalance(deployer)
+            // Test 
+            assert.equal(endFundmebalance, 0)
+            assert.equal(startingFundmebalance.add(startingDeployerbalance).toString(), endDeployerbalance.toString())
         })
     })
 })
